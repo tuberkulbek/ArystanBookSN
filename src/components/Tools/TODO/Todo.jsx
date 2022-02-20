ea1a78311c0f05ce5a9f08257db90490815e387d
tuberkulbek <a.burmaganov5@gmail.com>
2022-02-20 22:04:19 +0600

    19.02.22

--- src/components/Tools/TODO/Todo.jsx
+++ src/components/Tools/TODO/Todo.jsx
@@ -1,25 +1,9 @@
-import s from './Todo.module.css'
+import './Todo.css'
 
 const Todo = () => {
     return (
-        <div className={s.items}>
-            <table>
-                <tr>
-                    <th>Person 1</th>
-                    <th>Person 2</th>
-                    <th>Person 3</th>
-                </tr>
-                <tr>
-                    <td>Emil</td>
-                    <td>Tobias</td>
-                    <td>Linus</td>
-                </tr>
-                <tr>
-                    <td>16</td>
-                    <td>14</td>
-                    <td>10</td>
-                </tr>
-            </table>
+        <div class={"testing"}>
+            {'sdvsdv'}
         </div>
     );
 };
