3f5957474fc530e99cd60a43261355577063781b
tuberkulbek <a.burmaganov5@gmail.com>
2022-02-20 22:09:32 +0600

    20.02.22

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-import './App.css';
 import Header from "./components/Header/header";
 import Navbar from "./components/Navbar/nav-bar";
 import Tools from "./components/Tools/Tools";
+import './App.css'
 
 function App(props) {
     return (
@@ -15,24 +14,4 @@ function App(props) {
         </div>
     );
 }
-
-=======
-import './App.css';
-import Header from "./components/Header/header";
-import Navbar from "./components/Navbar/nav-bar";
-import Tools from "./components/Tools/Tools";
-
-function App(props) {
-    return (
-        <div className="container">
-            <Header/>
-            <Navbar/>
-            <div className="container-content">
-                <Tools store={props.store}/>
-            </div>
-        </div>
-    );
-}
-
->>>>>>> 065081c7a4cdfb8df1d24491aefe819d31dce183
 export default App;
